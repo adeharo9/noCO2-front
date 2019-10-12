875398faa74368ca23ad3851cf1d852b511fd9c1
Juan Miguel de Haro Ruiz <juanmiguel1996@gmail.com>
2019-10-12 16:20:59 +0200

    Show google maps after searching route

--- routeViewer.js
+++ routeViewer.js
@@ -1,7 +1,17 @@
+
+var map;
+
+window.initMap = function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    });
+};
+
 const drawRoute = (route) => {
     alert(JSON.stringify(route));
-    $('#form_calc').remove();
-    $('#container').html(JSON.stringify(route));
+    $('#form_calc').hide();
+    $('#map').show();
 };
 
 export { drawRoute };
