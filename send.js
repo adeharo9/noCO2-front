9f3c2ce7b719245ed0df17f2e02db90cf523ca01
jmbellodeharo <josemariabellodeharodev@gmail.com>
2019-10-12 12:29:48 +0200

    added port

--- send.js
+++ send.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         let destination = $('#destination').val();
         alert("Origen: " + origin + " Destination: " + destination);
             let parameters = { origin: origin, destination: destination };
-            $.get( "http://104.248.40.235/emissions/", parameters, function(data) {
+            $.get( "http://104.248.40.235:8080/emissions/", parameters, function(data) {
                 alert(JSON.stringify(data));
                 $('#form_calc').remove();
                 $('#container').html(JSON.stringify(data));
@@ -20,7 +20,7 @@ $(document).ready(function() {
         let message = $('#message').val();
         alert("Nombre: " + origin + " apellidos: " + surname + " email: " + email + " message: " + message);
         let parameters = { name: name, surname: surname, email: email, message: message };
-        $.get( "http://104.248.40.235/contact/", parameters, function(data) {
+        $.get( "http://104.248.40.235:8080/contact/", parameters, function(data) {
             alert(JSON.stringify(data));
             $('#form_calc').remove();
             $('#container').html("<h2>Thank you for contacting with us, we will give you a response as soon as possible!</h2>");
